e45c2e03594bd769757c31b353353ac30893f71e
smutallib97 <smutallib97@gmail.com>
2022-09-18 22:51:09 +0530

    Display Message

---
+++ Day1Problems/DisplayMessage.java
@@ -0,0 +1,8 @@
+package Day1Problems;
+
+public class DisplayMessage {
+    public static void main(String[] args) {
+        System.out.println("Hello BridgeLabzians");
+        System.out.println("This is Batch LFP - 202");
+    }
+}
