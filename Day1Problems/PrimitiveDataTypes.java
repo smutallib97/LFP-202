9064718df2ee4631365f48ebed156a2c396c0a93
smutallib97 <smutallib97@gmail.com>
2022-09-19 01:33:14 +0530

    PrimitiveDataTypes

---
+++ Day1Problems/PrimitiveDataTypes.java
@@ -0,0 +1,21 @@
+package Day1Problems;
+
+public class PrimitiveDataTypes {
+    static short s1;
+    static byte b1;
+    static int i1;
+    static long l1;
+    static float f1;
+    static double d1;
+    public static void main(String[] args) {
+
+
+        System.out.println("Short Value is: "+s1);
+        System.out.println("Byte Value is: "+b1);
+        System.out.println("Integer Value is: "+i1);
+        System.out.println("Long Value is: "+l1);
+        System.out.println("Float Value is: "+f1);
+        System.out.println("Double Value is: "+d1);
+
+    }
+}
